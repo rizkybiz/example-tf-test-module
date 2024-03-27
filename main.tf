4000fd44894998ca91dd92863664694914a254be
Justin DeFrank <justin.defrank@gmail.com>
2024-03-27 12:47:33 +0000

    upd: added terraform block

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
 resource "random_string" "random" {
   length           = var.random_string_length
   special          = true
